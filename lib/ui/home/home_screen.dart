12c052edf5bc393631f8da97aded511a17daf177
Vo Minh Son <145053920+sonvm31@users.noreply.github.com>
2026-06-03 00:09:05 +0700

    init folder struc

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -5,16 +5,10 @@ class HomeScreen extends StatefulWidget{
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-  int _currentIndex = 0;
 
-  final List<Widget> _tabs = [
-    const HomeTab(),
-    const Center(child: Text('Giỏ hàng')),
-    const Center(child: Text('Tin nhắn')),
-    const Center(child: Text('Cá nhân'))
-  ]
-}
