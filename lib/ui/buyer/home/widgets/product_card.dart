0d7fc4d35850088a526e0436341f18ca03dfa829
Vo Minh Son <145053920+sonvm31@users.noreply.github.com>
2026-06-05 14:13:07 +0700

    add product service

--- lib/ui/buyer/home/widgets/product_card.dart
+++ lib/ui/buyer/home/widgets/product_card.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sales_online_app/core/constants/app_styles.dart';
+import 'package:sales_online_app/data/models/product_model.dart';
 
 class ProductCard extends StatelessWidget {
-  final Map<String, String> product;
+  final ProductModel product;
 
   const ProductCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
+    final String formattedPrice =
+        "${product.price.toStringAsFixed(0).replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]}.')}đ";
 
     return Container(
       decoration: BoxDecoration(
@@ -27,66 +31,23 @@ class ProductCard extends StatelessWidget {
         children: [
           Expanded(
             child: Container(
-              margin: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: AppRadius.large,
-                image: DecorationImage(
-                  image: NetworkImage(product['image']!),
-                  fit: BoxFit.cover,
-                ),
+              margin: EdgeInsets.all(AppSpacing.sm),
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(borderRadius: AppRadius.large),
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                loadingBuilder: (context, child, loadingProgress){
+                  if(loadingProgress == null) return child;
+                  return Container(
+                    color: isDark ? AppColors.borderDark : AppColors.borderLight,
+                    child: const Center(child: Icon(Icons.image_not_supported_outlined),),
+                  )
+                },
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-                left: AppSpacing.md - 2,
-                right: AppSpacing.md - 2,
-                bottom: AppSpacing.md - 2),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product['name']!,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: AppTextStyles.bodyLarge.copyWith(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    color: isDark ? AppColors.textLight : AppColors.textDark,
-                  ),
-                ),
-                AppSpacing.h8,
-                Text(
-                  product['price']!,
-                  style: AppTextStyles.bodyLarge.copyWith(
-                    color: AppColors.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                AppSpacing.h8,
-                Row(
-                  children: [
-                    Icon(
-                      Icons.storefront,
-                      size: 14,
-                      color: isDark ? AppColors.textMutedDark : AppColors.textMutedLight
-                    ),
-                    AppSpacing.w4,
-                    Expanded(
-                        child: Text(
-                          product['store']!,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: AppTextStyles.caption.copyWith(
-                            color: isDark ? AppColors.textMutedDark : AppColors.textMutedLight,
-                          ),
-                        )
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
+          )
         ],
       ),
     );
