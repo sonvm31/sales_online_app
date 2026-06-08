010d1412d6aa20645a8cc31205c49c2750e5a0fa
thaivhm <hoangminhthaiv@gmail.com>
2026-06-08 09:45:34 +0700

    chore(auth): remove unused mock auth repository

--- lib/core/config/auth_config.dart
+++ lib/core/config/auth_config.dart
@@ -1,14 +1,8 @@
 import 'package:sales_online_app/data/repositories/api_auth_repository.dart';
 import 'package:sales_online_app/data/repositories/auth_repository.dart';
-import 'package:sales_online_app/data/repositories/mock_auth_repository.dart';
 
 class AuthConfig {
   AuthConfig._();
 
-  static bool get useMockAuth => false;
-
-  static AuthRepository createRepository() {
-    if (useMockAuth) return const MockAuthRepository();
-    return ApiAuthRepository();
-  }
+  static AuthRepository createRepository() => ApiAuthRepository();
 }
