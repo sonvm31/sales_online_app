1ba7a190175932ccec228e0138ac6a70a1794378
Truong Le Huy Hoang <hoanghuypeter1182004@gmail.com>
2026-06-17 09:01:06 +0700

    feat(auth): persist backend user id

--- lib/data/services/auth_session_service.dart
+++ lib/data/services/auth_session_service.dart
@@ -2,6 +2,7 @@ import 'package:sales_online_app/data/models/auth_session.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthSessionService {
+  static const String _userIdKey = 'auth_user_id';
   static const String _accessTokenKey = 'auth_access_token';
   static const String _firebaseUidKey = 'auth_firebase_uid';
   static const String _fullNameKey = 'auth_full_name';
@@ -25,6 +26,7 @@ class AuthSessionService {
     }
 
     return AuthSession(
+      userId: prefs.getInt(_userIdKey),
       accessToken: token,
       firebaseUid: firebaseUid,
       fullName: prefs.getString(_fullNameKey),
@@ -37,6 +39,10 @@ class AuthSessionService {
   Future<void> save(AuthSession session) async {
     final prefs = await SharedPreferences.getInstance();
     await Future.wait([
+      if (session.userId == null)
+        prefs.remove(_userIdKey)
+      else
+        prefs.setInt(_userIdKey, session.userId!),
       prefs.setString(_accessTokenKey, session.accessToken),
       prefs.setString(_firebaseUidKey, session.firebaseUid),
       prefs.setString(_emailKey, session.email),
@@ -55,6 +61,7 @@ class AuthSessionService {
   Future<void> clear() async {
     final prefs = await SharedPreferences.getInstance();
     await Future.wait([
+      prefs.remove(_userIdKey),
       prefs.remove(_accessTokenKey),
       prefs.remove(_firebaseUidKey),
       prefs.remove(_fullNameKey),
