1efdd7ce2351fae2f6c61322186b4ab7bbe995cf
thaivhm <hoangminhthaiv@gmail.com>
2026-07-02 10:38:41 +0700

    feat(seller): add seller mode page, new product page, products list page, orders list page

--- lib/data/services/category_service.dart
+++ lib/data/services/category_service.dart
@@ -5,17 +5,17 @@ import 'package:sales_online_app/data/models/category_model.dart';
 class CategoryService {
   final _dio = DioClient().dio;
 
-  Future<List<CategoryModel>> fetchCategories() async{
-    try{
+  Future<List<CategoryModel>> fetchCategories() async {
+    try {
       final response = await _dio.get('/categories');
-      if (response.statusCode == 200){
+      if (response.statusCode == 200) {
         final List<dynamic> data = response.data;
 
         return data.map((json) => CategoryModel.fromJson(json)).toList();
-      }else{
+      } else {
         throw Exception('Lỗi phản hồi hệ thống: ${response.statusCode}');
       }
-    }on DioException catch (e){
+    } on DioException catch (e) {
       throw Exception('Không thể kết nối đến máy chủ: ${e.message}');
     }
   }
